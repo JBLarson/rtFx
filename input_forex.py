3c8e0723e9fbd1b00cba13c5aa5bdd290d7d6808
Above the Book <58607747+AbovetheBook@users.noreply.github.com>
2020-06-29 23:21:58 -0400

    input values for execute forex v2

--- input_forex.py
+++ input_forex.py
@@ -1,7 +1,10 @@
 import numpy as np
 
-contract = "USD/JPY>"
-strike_range = np.arange(109.04, 110.04, .1)
+ro = lambda n : round(n, ndigits=2)
+rp = lambda n : round(n, ndigits=4)
+
+
+contract, stk_rng = "USD/JPY>", np.arange(109.04, 110.04, .1)
 
 b1str, b1avgp, b1size = float(109.25), 65, 10
 s1str, s1avgp, s1size = float(109.44), 45, 8
@@ -13,3 +16,8 @@ b1T = "Buy " + str(b1size) + " contracts of " + contract + str(b1str) + " @ " + 
 s1T = "Sell " + str(s1size) + " contracts of " + contract + str(s1str) + " @ " + str(s1avgp) + "$"
 
 b1T2, s1T2 = "Buy " + contract + str(b1str), "Sell " + contract + str(s1str)
+
+b1_rez = [b1w if stk >= b1str else -b1r for stk in stk_rng]
+s1_rez = [s1w if stk < abs(s1str) else -s1r for stk in stk_rng]
+
+net_rez = [b1_rez[i]+s1_rez[i] for i in range(len(b1_rez))]
